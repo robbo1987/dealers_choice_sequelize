988dd3424230392af0abec03a85f3a6d4b4afa28
Robert Albenda <ralbenda@gmail.com>
2022-02-14 12:34:04 -0500

    phase 0 complete

--- server.js
+++ server.js
@@ -1,19 +1,43 @@
-const Sequelize = require('sequelize')
-const sequelize = new Sequelize( process.env.DATABASE_URL || 'postgres://localhost/two_table' )
+const Sequelize = require("sequelize");
+const sequelize = new Sequelize(
+  process.env.DATABASE_URL || "postgres://localhost/two_table"
+);
 
+const Team = sequelize.define("team", {
+  name: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+});
 
+const Player = sequelize.define("player", {
+  name: {
+    type: Sequelize.DataTypes.STRING,
+    allowNull: false,
+    unique: true,
+  },
+});
 
-const start = async (req,res,next) => {
-   try{ 
-    console.log('hello world');
-   }
+const start = async () => {
+  try {
+    console.log("initiating");
+    await sequelize.sync({ force: true });
+    const Yankees = await Team.create({ name: "Yankees" });
+    const Mets = await Team.create({ name: "Mets" });
+    const Nets = await Team.create({ name: "Nets" });
+    const Knicks = await Team.create({ name: "Knicks" });
+    await Player.create({ name: "Julius Randle" });
+    await Player.create({ name: "Aaron Judge" });
+    await Player.create({ name: "Francisco Lindor" });
+    await Player.create({ name: "Kevin Durant" });
+    await Player.create({ name: "Gerrit Cole" });
+    await Player.create({ name: "Mitchell Robinson" });
+    await Player.create({ name: "Max Sxherzer" });
+    await Player.create({ name: "Kyrie Irving" });
+  } catch (ex) {
+    console.log(ex);
+  }
+};
 
-   catch(ex){
-    next(ex)
-
-   }
-}
-
-start()
-
-
+start();
